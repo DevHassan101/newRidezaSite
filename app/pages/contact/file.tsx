e2396c8ee0628c047fb25004becd32861a18fd74
Hassan Shaikh <147030057+DevHassan101@users.noreply.github.com>
2026-02-17 11:56:13 +0500

    second-commit

---
+++ app/pages/contact/file.tsx
@@ -0,0 +1,7 @@
+export default function file(){
+    return(
+        <>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sit explicabo dignissimos quam a deserunt.</p>
+        </>
+    )
+}
